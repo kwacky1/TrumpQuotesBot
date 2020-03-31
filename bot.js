63580d48303ff7ed5a94a38636b0c73cfa4cc06a
Glitch (kwacky1-trumpquotesbot) <none>
2020-03-31 10:36:00 +0000

    🎺🐂 Checkpoint ./watch.json:13753838/242 ./bot.js:13753838/331

--- bot.js
+++ bot.js
@@ -1,3 +1,15 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
